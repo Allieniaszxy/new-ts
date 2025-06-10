df80c4601b7c51e6b14931cf6bbeaca18c573be1
Allieniaszxy <alakaalamin40@gmail.com>
2025-06-10 07:12:00 -0700

    first typescript

---
+++ first.ts
@@ -0,0 +1,10 @@
+//we declare variable using annotation and inference in typescript
+//annotation can come direct either inference which is implicit  or direct which is explicit or annotation
+
+//annotation
+let nam: string = "ife";
+let a: number = 65;
+
+//inference
+let secname = "jackson";
+let b = 76;
